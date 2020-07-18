3057ae43682b6d7ed7ebade5337026219f1e094f
Tobias Meyer <tobiuhg@gmail.com>
2020-07-18 21:50:26 +0200

    finally a working cmake build and debug

--- Firmware/main.cpp
+++ Firmware/main.cpp
@@ -1,12 +1,24 @@
 #include <stdint.h>
-
+#define STM32F429xx
+#include "stm32f4xx.h"
+#include "stm32f4xx_ll_rcc.h"
+#include <stm32f4xx_ll_gpio.h>
 int main()
 {
-    volatile auto i = 1;
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOGEN;
+    volatile auto i = 10000;
+    while (i--)
+        ;
+    LL_GPIO_SetPinMode(GPIOG, LL_GPIO_PIN_13, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOG, LL_GPIO_PIN_14, LL_GPIO_MODE_OUTPUT);
+
     while(1)
     {
-        i++;
+        volatile auto i = 100000;
+        while(i--);
+        LL_GPIO_TogglePin(GPIOG,LL_GPIO_PIN_13);
+        LL_GPIO_TogglePin(GPIOG, LL_GPIO_PIN_14);
     }
-    
+
     return -1;
 }
